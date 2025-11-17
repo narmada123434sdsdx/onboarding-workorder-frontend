0e5b40888a6a9ee5488d03a32b49c2267502d8df
RaviKumar0509 <narmadar710@gmail.com>
2025-11-17 17:59:41 +0530

    fixed rollup deployment issue

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// ❌ Remove BrowserRouter
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-
+// ✅ Use HashRouter instead
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-    <Routes>
-      {/* User site */}
-      <Route path="/*" element={<App />} />
-
-    </Routes>
-  </BrowserRouter>
+    <HashRouter>
+      <Routes>
+        {/* User site */}
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>,
 );
