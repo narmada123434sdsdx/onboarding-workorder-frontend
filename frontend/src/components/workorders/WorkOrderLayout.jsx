ce7b46b70d66f8b30a2e6f2a9f206096891d11dc
RaviKumar0509 <narmadar710@gmail.com>
2025-11-17 17:42:44 +0530

    fixed file path and deployment issue

--- frontend/src/components/workorders/WorkOrderLayout.jsx
+++ frontend/src/components/workorders/WorkOrderLayout.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import ChildWorkorder from "./ChildWorkorder";
-import SearchWorkOrder from "./SearchWorkOrder";
+import SearchWorkOrder from "./SearchWorkorder";
 
 // import "./css/sidebar.css";  // if needed for sidebar
-import WorkOrderTypePage from "./Workorder_type";
-import WorkOrderAreaPage from "./Workorder_Area";
+import WorkorderTypePage from "./Workorder_type";
+import WorkorderAreaPage from "./Workorder_Area";
 import CreateWorkOrder from "./CreateWorkorder";
 import MappingPage from "./MappingPage";
 import WorkOrders from "./WorkOrder";
@@ -26,10 +26,10 @@ export default function WorkOrderLayout() {
           <Route path="/create-workorder" element={<CreateWorkOrder />} />
           <Route path="/child-workorder" element={<ChildWorkorder />} />
           <Route path="/mapping-workorder" element={<MappingPage />} />
-          <Route path="/workorder-type" element={<WorkOrderTypePage />} />
-          <Route path="/workorder-area" element={<WorkOrderAreaPage />} />                 
+          <Route path="/workorder-type" element={<WorkorderTypePage />} />
+          <Route path="/workorder-area" element={<WorkorderAreaPage />} />                 
           <Route path="/list" element={<WorkOrders/>} />
-          <Route path="/search-workorder" element={<SearchWorkOrder/>} />  
+          <Route path="/search-workorder" element={<SearchWorkorder/>} />  
           <Route path="/contractor" element={<Contractor />} />
           <Route path="/workorders/:id" element={<WorkOrderDetails />} />
         </Routes>
